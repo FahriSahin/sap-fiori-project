a1e35c4939583a8c02f801da9854d098ce9b69bf
Fahri Şahin <55fahrisahin55@gmail.com>
2025-09-05 11:48:41 +0000

    auth ve logout

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "project2/model/models"
-], (UIComponent, models) => {
+    "project2/model/models",
+    "sap/ui/model/json/JSONModel"
+], (UIComponent, models, JSONModel) => {
     "use strict";
 
     return UIComponent.extend("project2.Component", {
@@ -13,14 +14,35 @@ sap.ui.define([
         },
 
         init() {
-            // call the base component's init function
             UIComponent.prototype.init.apply(this, arguments);
 
-            // set the device model
+            // Device model
             this.setModel(models.createDeviceModel(), "device");
 
-            // enable routing
-            this.getRouter().initialize();
+            // Login modeli
+            if (!this.getModel("login")) {
+                const oLoginModel = new JSONModel({
+                    isLoggedIn: localStorage.getItem("isLoggedIn") === "true" ||
+                                sessionStorage.getItem("isLoggedIn") === "true"
+                });
+                this.setModel(oLoginModel, "login");
+            }
+
+            // Router
+            const oRouter = this.getRouter();
+            oRouter.initialize();
+
+            // Route guard
+            oRouter.attachBeforeRouteMatched((oEvent) => {
+                const bLoggedIn = this.getModel("login").getProperty("/isLoggedIn");
+                const sRouteName = oEvent.getParameter("name");
+
+                // Eğer login değilse ve route Login/Register değilse yönlendir
+                if (!bLoggedIn && sRouteName !== "Login" && sRouteName !== "Register") {
+                    oEvent.preventDefault(); // route değişimini durdur
+                    oRouter.navTo("Login");
+                }
+            });
         }
     });
 });
--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -1,55 +1,73 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ], function(Controller, MessageToast) {
-  "use strict";
+    "use strict";
 
-  return Controller.extend("project2.controller.Login", {
+    return Controller.extend("project2.controller.Login", {
 
-    onInit: function() {
-      // Eğer daha önce kayıtlı kullanıcı yoksa, default admin ekle
-      if (!localStorage.getItem("Users")) {
-        const defaultUsers = [
-          { username: "admin", password: "1234" }
-        ];
-        localStorage.setItem("Users", JSON.stringify(defaultUsers));
-      }
-    },
+        onInit: function() {
+            // Default admin kullanıcı
+            if (!localStorage.getItem("Users")) {
+                const defaultUsers = [{ username: "admin", password: "1234" }];
+                localStorage.setItem("Users", JSON.stringify(defaultUsers));
+            }
+        },
 
-    onLogin: function() {
-      const sUsername = this.byId("usernameInput").getValue();
-      const sPassword = this.byId("passwordInput").getValue();
+        onLogin: function() {
+            const sUsername = this.byId("usernameInput").getValue();
+            const sPassword = this.byId("passwordInput").getValue();
+            const bRememberMe = this.byId("rememberMeCheckBox").getSelected();
 
-      // LocalStorage'dan kullanıcıları al
-      const aUsers = JSON.parse(localStorage.getItem("Users")) || [];
+            const aUsers = JSON.parse(localStorage.getItem("Users")) || [];
+            const oUser = aUsers.find(u => u.username === sUsername && u.password === sPassword);
 
-      const oUser = aUsers.find(u => u.username === sUsername && u.password === sPassword);
+            if (oUser) {
+                const oLoginModel = this.getOwnerComponent().getModel("login");
 
-      if (oUser) {
-        MessageToast.show(this.getView().getModel("i18n").getProperty("loginSuccess"));
-        this.getOwnerComponent().getRouter().navTo("Main");
-      } else {
-        MessageToast.show(this.getView().getModel("i18n").getProperty("loginFailed"));
-      }
-    },
+                if (bRememberMe) {
+                    localStorage.setItem("isLoggedIn", "true");
+                    localStorage.setItem("username", sUsername);
+                } else {
+                    sessionStorage.setItem("isLoggedIn", "true");
+                    sessionStorage.setItem("username", sUsername);
+                }
 
-    onGoToRegister: function() {
-      this.getOwnerComponent().getRouter().navTo("Register");
-    },
+                oLoginModel.setProperty("/isLoggedIn", true);
 
-    onLanguageChange: function(oEvent) {
-    var sSelectedKey = oEvent.getParameter("selectedItem").getKey(); // "en" veya "tr"
-    var oView = this.getView();
+                MessageToast.show(this.getView().getModel("i18n").getProperty("loginSuccess"));
+                this.getOwnerComponent().getRouter().navTo("Main");
+            } else {
+                MessageToast.show(this.getView().getModel("i18n").getProperty("loginFailed"));
+            }
+        },
 
-    if (sSelectedKey === "tr") {
-        var oModelTR = this.getOwnerComponent().getModel("i18n_tr");
-        oView.setModel(oModelTR, "i18n");
-    } else {
-        var oModelEN = this.getOwnerComponent().getModel("i18n");
-        oView.setModel(oModelEN, "i18n");
-    }
-}
+        onGoToRegister: function() {
+            this.getOwnerComponent().getRouter().navTo("Register");
+        },
 
+        onLanguageChange: function(oEvent) {
+            const sSelectedKey = oEvent.getParameter("selectedItem").getKey();
+            const oView = this.getView();
 
-  });
+            if (sSelectedKey === "tr") {
+                oView.setModel(this.getOwnerComponent().getModel("i18n_tr"), "i18n");
+            } else {
+                oView.setModel(this.getOwnerComponent().getModel("i18n"), "i18n");
+            }
+        },
+
+        onLogout: function() {
+            // Hem local hem session temizle
+            localStorage.removeItem("isLoggedIn");
+            localStorage.removeItem("username");
+            sessionStorage.removeItem("isLoggedIn");
+            sessionStorage.removeItem("username");
+
+            this.getOwnerComponent().getModel("login").setProperty("/isLoggedIn", false);
+
+            this.getOwnerComponent().getRouter().navTo("Login");
+            MessageToast.show(this.getView().getModel("i18n").getProperty("logoutSuccess") || "Logged out");
+        }
+    });
 });
